7e786d0e601b2cd619b4a55aadc108401e6024a3
jeong5728 <jeong5728@gmail.com>
2022-06-16 21:21:19 +0900

    pref: scroll transition css 만으로 해결

--- src/layouts/content-layout.jsx
+++ src/layouts/content-layout.jsx
@@ -6,32 +6,16 @@ import * as style from "assets/styles/layouts/content-layout.module.scss";
 const ContentLayout = ({ children, data }) => {
   // dark mode
   const { theme } = useContext(ThemeContext);
-  // divide mobile pc
-  useEffect(() => {
-    const isMobile = () => {
-      try {
-        document.createEvent("TouchEvent");
-        return true;
-      } catch (e) {
-        return false;
-      }
-    }
-    if(isMobile()) {
-      document.querySelector('#scroll > div').style.border = '0';
-    }
-  }, [])
   return (
     <div id="scroll" className={!theme ? style.layout : style.layoutDark}>
-      <div className={style.border}>
-        <div className={style.maxWidth}>
-          <div className={style.minHeight}>
-            <div className={style.padding}>
-              {children}
-            </div>
+      <div className={style.maxWidth}>
+        <div className={style.minHeight}>
+          <div className={style.padding}>
+            {children}
           </div>
         </div>
-        <Footer data={data}/>
       </div>
+      <Footer data={data}/>
     </div>
   );
 }
