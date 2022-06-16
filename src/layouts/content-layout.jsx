df8b79080599a05e48025f6247e1e2e58f341d2c
jeong5728 <jeong5728@gmail.com>
2022-06-16 20:15:47 +0900

    feat: 모바일 접속 시 scroll border 제거

--- src/layouts/content-layout.jsx
+++ src/layouts/content-layout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ThemeContext } from "contexts/theme";
 import Footer from "components/footer";
 import * as style from "assets/styles/layouts/content-layout.module.scss";
@@ -6,6 +6,20 @@ import * as style from "assets/styles/layouts/content-layout.module.scss";
 const ContentLayout = ({ children, data }) => {
   // dark mode
   const { theme } = useContext(ThemeContext);
+  // divide mobile pc
+  useEffect(() => {
+    const isMobile = () => {
+      try {
+        document.createEvent("TouchEvent");
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+    if(isMobile()) {
+      document.querySelector('#scroll > div').style.border = '0';
+    }
+  }, [])
   return (
     <div id="scroll" className={!theme ? style.layout : style.layoutDark}>
       <div className={style.border}>
